66b5a218c5ddf602ca3a91f7de320e67a3d403af
Marcel <ligimixi@gmail.com>
2024-12-11 11:50:57 +0100

    sdwrite routine (grimm raw data) & gps routine & oled debug msg

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include <U8g2lib.h>
 #include <LoRa.h>
 #include <SdFat.h>
+#include <Adafruit_GPS.h>
 
 // PIN Connections ----------------------------------------------------------
 #define grimm_RX 16       // Grimm RX
@@ -20,13 +21,18 @@
 #define LORA_DIO0 2
 #define LORA_RST 3
 #define LORA_CS 8
+//SD CS PIN (SPI)
 #define SD_CS_PIN 46
+//GPS RX & TX PINS
+#define GPS_RX 4 // µC RX not GPS RX (GPS TX -> µC RX)
+#define GPS_TX 5 // µC TX not GPS TX (GPS RX -> µC TX)
 // BME280 Vin -> 3.3V; SCK -> SCL; SDI -> SDA; GND -> GND
 //---------------------------------------------------------------------------
 
 //GRIMM varaiables ----------------------------------------------------------
 const int MAX_LINES = 4;
 int dataIndex = 0;
+bool newGrimmValuesReceived = false;
 //---------------------------------------------------------------------------
 
 // Struct for the sensor data------------------------------------------------
@@ -51,6 +57,14 @@ HardwareSerial grimmSerial(2);
 //----------------------------------------------------------------------------
 // SW Serial for the partector2 ----------------------------------------------
 EspSoftwareSerial::UART partectorSerial;
+//----------------------------------------------------------------------------
+
+// SW Serial for the GPS and object creation ---------------------------------
+EspSoftwareSerial::UART gpsSerial;
+Adafruit_GPS GPS(&gpsSerial);
+#define GPSECHO false
+bool newNMEAreceived = false;
+bool parsingFailed = false;
 //----------------------------------------------------------------------------
 
 // BME280 sensor -------------------------------------------------------------
@@ -92,6 +106,14 @@ void processCO2Data(void);
 void drawData2OLED(void);
 void sendLoRaData(void);
 void writeData2SD(void);
+float gpsDecimal(float gps, char sector);
+void gpsRunFrequently(void);
+void gpsWait4Fix(void);
+String getFilename(void);
+void oledLoraErr(void);                     // Display error message on OLED
+void oledSDErr(void);
+void oledBMEErr(void);
+void oledCO2Err(void);
 //----------------------------------------------------------------------------
 
 // TaskScheduler setup -------------------------------------------------------
@@ -102,6 +124,7 @@ Task t_CO2(1000, TASK_FOREVER, &processCO2Data); //CO2 Data is only available ev
 Task t_OLED(1000, TASK_FOREVER, &drawData2OLED);
 Task t_LoRa(2000, TASK_FOREVER, &sendLoRaData);
 Task t_SD(1000, TASK_FOREVER, &writeData2SD);
+Task t_gpsRunFrequently(TASK_IMMEDIATE, TASK_FOREVER, &gpsRunFrequently);
 Scheduler runner;
 //----------------------------------------------------------------------------
 
@@ -109,39 +132,61 @@ void setup() {
   Serial.begin(9600);
   grimmSerial.begin(9600, SERIAL_8N1, grimm_RX, -1);
   partectorSerial.begin(38400, EspSoftwareSerial::SWSERIAL_8N1, partector_RX, -1, false, 256);
+  gpsSerial.begin(9600, EspSoftwareSerial::SWSERIAL_8N1, GPS_RX, GPS_TX, false, 256);
+
+  //OLED
+  Wire.begin();
+  u8g2.begin();
+  u8g2.setFont(u8g2_font_tiny5_tr);
+  u8g2.clearBuffer();
+
+  //GPS
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_5HZ);
+  delay(1000);
+  gpsSerial.println(PMTK_Q_RELEASE);
+  ESP_LOGD("GPS", "GPS initialized, waiting for fix!");
+
+  //wait for GPS fix
+  gpsWait4Fix();
 
   //LoRa init
   SPI.begin(SCK, MISO, MOSI, LORA_CS);
   LoRa.setPins(LORA_CS, LORA_RST, LORA_DIO0);
   if (!LoRa.begin(BAND)) {
     ESP_LOGD("LoRa init", "Starting LoRa failed!");
+    oledLoraErr();
   }
   ESP_LOGD("LoRa init", "LoRa init done!");
-
-  Wire.begin();
-
-  u8g2.begin();
-  u8g2.setFont(u8g2_font_tiny5_tr);
-  u8g2.clearBuffer();
+  //set LoRa syncWord
+  LoRa.setSyncWord(0xB2);
+  //enable CRC check
+  LoRa.enableCrc();
+  LoRa.setTxPower(20, PA_OUTPUT_PA_BOOST_PIN);
 
   //init BME280
   if (!bme.begin(0x77)) {
     ESP_LOGD("BME280", "Could not find a valid BME280 sensor, check wiring!");
+    oledBMEErr();
   }
 
   //scd30 init
   if (co2.begin() == false) {
     ESP_LOGD("SCD30", "Sensor not detected. Please check your wiring.");
+    oledCO2Err();
   }
 
   //SD Card init
-  filename = "test1.csv";
+  filename = getFilename();
   if (!sd.begin(SD_CS_PIN, SPI_FULL_SPEED)) {
     ESP_LOGD("SD Card", "SD Card init failed!");
+    oledSDErr();
+    sd.initErrorHalt();
+    return;
   }
 
   if (sd.exists(filename.c_str())) {
-    ESP_LOGD("SD Card cecks", "File exists!");
+    ESP_LOGD("SD Card checks", "File exists!");
   }
   else {
     // create file
@@ -149,11 +194,20 @@ void setup() {
       ESP_LOGD("SD Card File Creation", "File creation failed!");
     } else {
       ESP_LOGD("SD Card File Creation", "File created!");
-       file.println("Datetime,Temperature,RedServoStatus,BlueServoStatus,Latitude,Longitude");
+      String csvHeader = "Datetime,Latitude,Longitude,Temperature,Humidity,Pressure,Altitude,CO2,PartNum,PartDiam,PartMass,";
+      for (int i(0); i < 31; ++i) {
+        if (i == 30) {
+          csvHeader += "Grimm" + String(i+1);
+        } else {
+          csvHeader += "Grimm" + String(i+1) + ",";
+        }
+      }
+      file.println(csvHeader);
     }
 
     if (!file.sync() || file.getWriteError()) {
       ESP_LOGD("SD Card File Sync", "File sync failed!");
+      oledSDErr();
     }
     file.close();
   }
@@ -168,6 +222,7 @@ void setup() {
   runner.addTask(t_OLED);
   runner.addTask(t_LoRa);
   runner.addTask(t_SD);
+  runner.addTask(t_gpsRunFrequently);
 
   t_Grimm2Struct.enable();
   t_Partector2.enable();
@@ -176,6 +231,7 @@ void setup() {
   t_OLED.enable();
   t_LoRa.enable();
   t_SD.enable();
+  t_gpsRunFrequently.enable();
 
 }
 
@@ -294,6 +350,11 @@ void processGrimmData(void) {
         linesRead++;
       }
     }
+    // set flag for new data
+    if (validSet && linesRead == MAX_LINES) {
+      newGrimmValuesReceived = true;
+    }
+    // Debug-Ausgabe
     #if CORE_DEBUG_LEVEL >= 4
         if (validSet && linesRead == MAX_LINES) {
           // Ausgabe des Arrays über den seriellen Monitor
@@ -359,7 +420,8 @@ void processBMEData(void) {
   data.temperature = bme.readTemperature();
   data.humidity = bme.readHumidity();
   data.pressure = bme.readPressure() / 100.0F;
-  data.altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  //data.altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  data.altitude = GPS.altitude;
   #if CORE_DEBUG_LEVEL >= 4
     Serial.printf("Temperature: %f\n", data.temperature);
     Serial.printf("Humidity: %f\n", data.humidity);
@@ -378,8 +440,9 @@ void processCO2Data(void) {
 void drawData2OLED(void) {
   u8g2.clearBuffer();
   u8g2.setDisplayRotation(U8G2_R1);
+  u8g2.setFont(u8g2_font_tiny5_tr);
   u8g2.setCursor(0, 10);
-  u8g2.print("Temperature:");
+  u8g2.print("Temp:");
   u8g2.setCursor(0, 20);
   u8g2.print(data.temperature);
   u8g2.print("C");
@@ -397,6 +460,14 @@ void drawData2OLED(void) {
   u8g2.setCursor(0, 80);
   u8g2.print(data.co2);
   u8g2.print("ppm");
+  u8g2.setCursor(0, 90);
+  u8g2.print("Time:");
+  u8g2.setCursor(0, 100);
+  u8g2.print(GPS.hour);
+  u8g2.print(":");
+  u8g2.print(GPS.minute);
+  u8g2.print(":");
+  u8g2.print(GPS.seconds);
   u8g2.sendBuffer();
 }
 
@@ -410,11 +481,28 @@ void sendLoRaData(void) {
 void writeData2SD(void) {
   
   if (!file.open(filename.c_str(), O_WRONLY | O_CREAT | O_AT_END)) {
-    ESP_LOGD("SDWrite Open Fiel", "File open failed!");
+    ESP_LOGD("SDWrite Open File", "File open failed!");
   }
 
   // write data to file
-  String sdData = String(data.temperature) + "," + String(data.humidity) + "," + String(data.pressure) + "," + String(data.altitude) + "," + String(data.co2) + "," + String(data.partectorNumber) + "," + String(data.partectorDiam) + "," + String(data.partectorMass);
+  String sdData = String(GPS.year) + "." + String(GPS.month) + "." + String(GPS.day) + "T";
+  sdData += String(GPS.hour) + "." + String(GPS.minute) + "." + String(GPS.seconds) + ",";
+  sdData += String(gpsDecimal(GPS.latitude, GPS.lat), 6) + "," + String(gpsDecimal(GPS.longitude, GPS.lon), 6) + ",";
+  sdData += String(data.temperature) + "," + String(data.humidity) + "," + String(data.pressure) + "," + String(data.altitude) + "," + String(data.co2) + "," + String(data.partectorNumber) + "," + String(data.partectorDiam) + "," + String(data.partectorMass) + ",";
+  if (newGrimmValuesReceived) {
+    for (int i(0); i < 31; ++i) {
+      if (i == 30) {
+        sdData += String(data.grimmValues[i]);
+      } else {
+        sdData += String(data.grimmValues[i]) + ",";
+      }
+    }
+    newGrimmValuesReceived = false;
+  } else {
+    for (int i(0); i < 30; ++i) {
+      sdData += ",";
+    }
+  }
   file.println(sdData);
 
   if (!file.sync() || file.getWriteError()) {
@@ -424,4 +512,83 @@ void writeData2SD(void) {
   file.close();
   ESP_LOGD("SDWrite", "Data written to SD Card!");
 
+}
+
+float gpsDecimal(float gps, char sector) {
+  float decimal = 0;
+  int degrees = int(gps / 100);
+  float minutes = gps - (degrees * 100);
+  decimal = degrees + (minutes / 60);
+  if (sector == 'S' || sector == 'W') {
+    decimal *= -1;
+  }
+  return decimal;
+}
+
+void gpsRunFrequently(void) {
+  char c = GPS.read();
+  if (GPS.newNMEAreceived()) {
+    newNMEAreceived = true;
+    if (!GPS.parse(GPS.lastNMEA())) {
+      parsingFailed = true;
+    }
+    else {
+      parsingFailed = false;
+    }
+  }
+}
+
+//wait for gps fix
+void gpsWait4Fix(void) {
+  u8g2.clearBuffer();
+  u8g2.setDisplayRotation(U8G2_R2);
+  u8g2.setFont(u8g2_font_logisoso28_tr);
+  u8g2.drawStr(4, 29, "WAIT4FIX");
+  u8g2.sendBuffer();
+  while (!GPS.fix) {
+    gpsRunFrequently();
+  }
+}
+
+//display lora err
+void oledLoraErr(void) {
+  u8g2.clearBuffer();
+  u8g2.setDisplayRotation(U8G2_R2);
+  u8g2.setFont(u8g2_font_logisoso28_tr);
+  u8g2.drawStr(4, 29, "LORA ERR");
+  u8g2.sendBuffer();
+}
+
+//display sd err
+void oledSDErr(void) {
+  u8g2.clearBuffer();
+  u8g2.setDisplayRotation(U8G2_R2);
+  u8g2.setFont(u8g2_font_logisoso28_tr);
+  u8g2.drawStr(4, 29, "SD ERR");
+  u8g2.sendBuffer();
+}
+
+//display bme280 err
+void oledBMEErr(void) {
+  u8g2.clearBuffer();
+  u8g2.setDisplayRotation(U8G2_R2);
+  u8g2.setFont(u8g2_font_logisoso28_tr);
+  u8g2.drawStr(4, 29, "BME ERR");
+  u8g2.sendBuffer();
+}
+
+//display co2 err
+void oledCO2Err(void) {
+  u8g2.clearBuffer();
+  u8g2.setDisplayRotation(U8G2_R2);
+  u8g2.setFont(u8g2_font_logisoso28_tr);
+  u8g2.drawStr(4, 29, "CO2 ERR");
+  u8g2.sendBuffer();
+}
+
+//get datetime for filename
+String getFilename(void) {
+  String filename = "";
+  filename += String(GPS.year)+ "-" + String(GPS.month)+ "-" + String(GPS.day) + "T" + String(GPS.hour) + "-" + String(GPS.minute) + "-" + String(GPS.seconds) + ".csv";
+  return filename;
 }
